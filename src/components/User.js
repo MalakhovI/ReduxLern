438e52bcf2e6abfea33a68c37fd2c4452305204e
MalakhovI <malakhov.ivan.1986@gmail.com>
2016-05-18 23:28:52 +0300

    server/client communication

--- src/components/User.js
+++ src/components/User.js
@@ -7,7 +7,7 @@ export default class User extends Component {
   render() {
     const { name } = this.props
     return <div className='ib user'>
-      <p>Привет, {name}!</p>
+      <p>Привет , {name}!</p>
     </div>
   }
 }
